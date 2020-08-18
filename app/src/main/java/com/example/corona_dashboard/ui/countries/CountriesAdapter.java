907537cdb5b4347b1b64fb873808536ce5645290
Thomas Dorfer <info@thomasdorfer.com>
2020-08-18 12:29:58 +0200

    add ui - CountriesAdapter; update CountriesFragment

---
+++ app/src/main/java/com/example/corona_dashboard/ui/countries/CountriesAdapter.java
@@ -0,0 +1,70 @@
+package com.example.corona_dashboard.ui.countries;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.corona_dashboard.R;
+import com.example.corona_dashboard.data.CountryStatistic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.CountryHolder> {
+    private List<CountryStatistic> countryStatisticList = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public CountryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_country, parent, false);
+        return new CountryHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CountryHolder holder, int position) {
+        CountryStatistic currentCountryStatistic = countryStatisticList.get(position);
+        holder.countryName.setText(currentCountryStatistic.getName());
+        holder.countryTodayCases.setText(String.valueOf(currentCountryStatistic.getTodayCases()));
+        holder.countryTodayDeaths.setText(String.valueOf(currentCountryStatistic.getTodayDeaths()));
+        holder.countryTodayRecovered.setText(String.valueOf(currentCountryStatistic.getTodayRecovered()));
+        holder.countryPopulation.setText(String.valueOf(currentCountryStatistic.getPopulation()));
+    }
+
+    public void setCountries(List<CountryStatistic> countryStatistics){
+        this.countryStatisticList = countryStatistics;
+        notifyDataSetChanged();
+    }
+
+    public void addCountries(CountryStatistic countryStatistic){
+        this.countryStatisticList.add(countryStatistic);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return countryStatisticList.size();
+    }
+
+    class CountryHolder extends RecyclerView.ViewHolder{
+        private TextView countryName;
+        private TextView countryTodayCases;
+        private TextView countryTodayDeaths;
+        private TextView countryTodayRecovered;
+        private TextView countryPopulation;
+
+        public CountryHolder(@NonNull View itemView) {
+            super(itemView);
+            this.countryName = itemView.findViewById(R.id.country_name);
+            this.countryTodayCases = itemView.findViewById(R.id.country_today_cases);
+            this.countryTodayDeaths = itemView.findViewById(R.id.country_today_deaths);
+            this.countryTodayRecovered = itemView.findViewById(R.id.counrty_today_recovered);
+            this.countryPopulation = itemView.findViewById(R.id.country_population);
+        }
+    }
+}
--- app/src/main/java/com/example/corona_dashboard/ui/countries/CountriesFragment.java
+++ app/src/main/java/com/example/corona_dashboard/ui/countries/CountriesFragment.java
@@ -4,34 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.corona_dashboard.R;
-import com.example.corona_dashboard.ui.continents.ContinentsFragment;
-import com.example.corona_dashboard.ui.continents.ContinentsViewModel;
+import com.example.corona_dashboard.data.CountryStatistic;
+import com.example.corona_dashboard.service.NetworkService;
+
+import java.util.List;
 
 public class CountriesFragment extends Fragment {
     private static String TAG = CountriesFragment.class.getSimpleName();
     private CountriesViewModel countriesViewModel;
+    private List<CountryStatistic> countryStatistics;
+    private CountriesAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         countriesViewModel = new ViewModelProvider(this).get(CountriesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_countries, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        countriesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        RecyclerView recyclerView = root.findViewById(R.id.country_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        recyclerView.setHasFixedSize(true);
+
+        adapter = new CountriesAdapter();
+        recyclerView.setAdapter(adapter);
+
+        NetworkService.getInstance(root.getContext());
+
+        NetworkService.getInstance().getAllCountriesStatistics(response -> adapter.addCountries(response));
+
+        getActivity().findViewById(R.id.btn_refresh).setVisibility(View.VISIBLE);
+        getActivity().findViewById(R.id.btn_refresh).setOnClickListener(click -> NetworkService.getInstance().getAllCountriesStatistics(response -> adapter.addCountries(response)));
+
         return root;
     }
 }
